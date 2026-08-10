a07f6262f73373aeb38006d39b325a80eb2dc8ab
Ayşenaz <aysenaz.kont80@gmail.com>
2026-08-10 21:51:38 +0300

    feat: add search, category state and reactive filteredProductsProvider

--- lib/features/products/presentation/providers/product_provider.dart
+++ lib/features/products/presentation/providers/product_provider.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mini_ecommerce_app/core/network/api_client.dart';
-import 'package:mini_ecommerce_app/features/products/data/models/product_model.dart';
 import 'package:mini_ecommerce_app/features/products/data/repositories/product_repository.dart';
+import '../../data/models/category_model.dart';
+import '../../data/models/product_model.dart';
 
 final productRepositoryProvider = Provider<ProductRepository>((ref) {
   final apiClient = ref.watch(apiClientProvider); 
@@ -16,4 +17,30 @@ final productsFutureProvider = FutureProvider<List<ProductModel>>((ref) async {
 final productDetailProvider = FutureProvider.family<ProductModel, int>((ref, id) async {
   final repository = ref.watch(productRepositoryProvider);
   return repository.fetchProductDetail(id);
+});
+
+final searchQueryProvider = StateProvider<String>((ref) => '');
+
+final selectedCategoryProvider = StateProvider<String?>((ref) => null);
+
+final categoriesProvider = FutureProvider<List<CategoryModel>>((ref) async {
+  final repository = ref.watch(productRepositoryProvider);
+  return repository.fetchCategories();
+});
+
+final filteredProductsProvider = FutureProvider<List<ProductModel>>((ref) async {
+  final repository = ref.watch(productRepositoryProvider);
+  
+  final query = ref.watch(searchQueryProvider);
+  final category = ref.watch(selectedCategoryProvider);
+
+  if (query.trim().isNotEmpty) {
+    return repository.searchProducts(query.trim());
+  }
+
+  if (category != null && category.isNotEmpty) {
+    return repository.fetchProductsByCategory(category);
+  }
+
+  return repository.fetchProducts();
 });
